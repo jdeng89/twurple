0fded4eca6567563b258ccd4a8312a09919c7e81
Jason Deng <AznInvasion@gmail.com>
2024-03-30 18:51:32 -0400

    Update types.ts
    Add 300 second Commercial

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,7 +1,7 @@
 /**
  * The possible lengths of a channel commercial.
  */
-export type CommercialLength = 30 | 60 | 90 | 120 | 150 | 180;
+export type CommercialLength = 30 | 60 | 90 | 120 | 150 | 180 | 300;
 
 /**
  * The type of a user.
